9828e802b8db426739d08e5f18f4cadc3949353d
ahribori <ahribori@gmail.com>
2019-07-13 00:54:59 +0900

    topojson으로 남한 지도 그리기

--- src/pages/geo/sample.tsx
+++ src/pages/geo/sample.tsx
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
+import * as topojson from 'topojson';
+import './sample.scss';
+
+const koreaMap = require('../../topojson/skorea-provinces-2018-topo-simple.json');
+const geojson = (topojson.feature(koreaMap, koreaMap.objects.skorea_provinces_2018_geo) as any)
+  .features;
 
 class Sample extends Component {
   render() {
-    return (
-      <div>
-        Sample
-      </div>
-    );
+    return <div className="d3" />;
+  }
+
+  renderMap() {
+    const width = 800;
+    const height = 800;
+
+    const svg = d3
+      .select('.d3')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    let projection = d3
+      .geoMercator()
+      .center([128, 36])
+      .scale(5000)
+      .translate([width / 2, height / 2]);
+
+    const path = d3.geoPath().projection(projection);
+
+    svg
+      .append('g')
+      .selectAll('path')
+      .data(geojson)
+      .enter()
+      .append('path')
+      .attr('d', path);
+  }
+
+  componentDidMount(): void {
+    this.renderMap();
   }
 }
 
