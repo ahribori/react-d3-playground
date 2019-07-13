6b2ccc48f98e9b1397ac5d72ce82670f191c35ff
ahribori <ahribori@gmail.com>
2019-07-14 00:20:17 +0900

    지도에 좌표찍기

--- src/pages/geo/sample.tsx
+++ src/pages/geo/sample.tsx
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
 import './sample.scss';
 import KoreaMap from '../../components/KoreaMap';
+import SeoulMap from '../../components/SeoulMap';
 
 class Sample extends Component {
-  state = {
-    scale: 3000
-  };
-
   render() {
-    const { scale } = this.state;
-
     return (
       <div
-        onWheel={e => {
-          const nextValue = scale - e.deltaY * 10;
-          if (nextValue >= 1000 && nextValue <= 10000) {
-            this.setState({ scale: nextValue });
-          }
-        }}
       >
-        <h3>Scale: {scale}</h3>
-        <KoreaMap scale={scale} translate={[0, 0]} />
+        <KoreaMap scale={5500} translate={[0, 0]} />
+        <SeoulMap scale={6000} translate={[0, 0]} />
       </div>
     );
   }
